3b84bc43046c3a0358300fcff78cd136057a27e6
Nina <Nino4ka11021988@gmail.com>
2022-02-09 12:03:43 +0300

    Main

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -3,6 +3,40 @@ import java.util.Scanner;
 
 public class TicTacToe {
     public static void main(String[] args) {
+        System.out.println("Use the following mapping table to specify a cell using numbers from 1 to 9:");
+        printTableMapping();
+        char[][] gameTable = {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
+        if (new Random().nextBoolean()) {
+            makeComputerMove(gameTable);
+            printGameTable(gameTable);
+        }
+        while (true) {
+            makeUserMove(gameTable);
+            printGameTable(gameTable);
+            if (isUserWin(gameTable)) {
+                System.out.println("YOU WIN!");
+                break;
+            }
+            if (isDraw(gameTable)) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+            makeComputerMove(gameTable);
+            printGameTable(gameTable);
+            if (isComputerWin(gameTable)) {
+                System.out.println("COMPUTER WIN!");
+                break;
+            }
+            if (isDraw(gameTable)) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+        }
+        System.out.println("GAME OVER!");
 
     }
 
@@ -97,10 +131,7 @@ public class TicTacToe {
         if (gameTable[0][0] == gameTable[1][1] && gameTable[0][0] == gameTable[2][2] && gameTable[0][0] == ch) {
             return true;
         }
-        if (gameTable[0][2] == gameTable[1][1] && gameTable[0][2] == gameTable[2][0] && gameTable[0][2] == ch) {
-            return true;
-        }
-        return false;
+        return gameTable[0][2] == gameTable[1][1] && gameTable[0][2] == gameTable[2][0] && gameTable[0][2] == ch;
     }
 
     private static boolean isDraw(char[][] gameTable) {
