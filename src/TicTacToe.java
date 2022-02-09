02c7c84d3ef8873265aaa135603f13937231f0e3
Nina <Nino4ka11021988@gmail.com>
2022-02-09 11:52:03 +0300

    Create method printGameTable

---
+++ src/TicTacToe.java
@@ -0,0 +1,16 @@
+public class TicTacToe {
+    public static void main(String[] args) {
+
+    }
+
+    private static void printGameTable(char[][] gameTable) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + gameTable[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------");
+    }
+}
